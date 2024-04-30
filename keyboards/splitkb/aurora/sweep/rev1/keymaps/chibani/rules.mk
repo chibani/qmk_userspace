9dd0605f125a2f9c35e9ebef9fb95661b7a8f75d
chibani <gerbaudloic@gmail.com>
2024-04-30 07:55:34 +0200

    fix userspace building

--- keyboards/splitkb/aurora/sweep/rev1/keymaps/chibani/rules.mk
+++ keyboards/splitkb/aurora/sweep/rev1/keymaps/chibani/rules.mk
@@ -13,5 +13,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-DEFAULT_FOLDER = splitkb/aurora/sweep/rev1
 TAP_DANCE_ENABLE = yes
